4fdb8a185ff569af1d8d4b405e6f7d640d32ba6a
bryan <bryandf.1996@gmail.com>
2021-05-04 20:08:02 +0200

    Primera subida a github

---
+++ app/src/main/java/com/ochoa/bryan/findtattoo/MainActivity.java
@@ -0,0 +1,79 @@
+package com.ochoa.bryan.findtattoo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mFirebaseAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    public static final int REQUEST_CODE = 54654;
+
+    List<AuthUI.IdpConfig> provider = Arrays.asList(
+            new AuthUI.IdpConfig.FacebookBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build()
+    );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    Toast.makeText(MainActivity.this, "Inicio de sesion OK!", Toast.LENGTH_SHORT).show();
+                } else {
+                    startActivityForResult(AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(provider)
+                            .setIsSmartLockEnabled(false)
+                            .build(), REQUEST_CODE
+                    );
+                }
+
+            }
+        };
+
+
+    }//END OF ONCREATE!
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    public void closeSession(View view) {
+        AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(MainActivity.this, "Sesión Cerrada", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+}
